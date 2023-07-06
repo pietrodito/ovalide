66fe7610e80f0867c27233a04d2a29be4bb797eb
Pierre Balaye <pierre@balaye.fr>
2023-07-06 13:03:15 +0000

    first commit

---
+++ R/read_score_file.R
@@ -0,0 +1,8 @@
+read_score_file <- function(csv_filepath,
+                            champ = c("mco", "had", "ssr", "psy"),
+                            statut = c("dgf", "oqn")) {
+  readr::read_csv2(csv_filepath)
+}
+
+
+
