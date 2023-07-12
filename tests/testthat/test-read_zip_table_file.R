da524e5eb3e1db30d5f6fadb55fbebded60b1e5e
Pierre Balaye <pierre@balaye.fr>
2023-07-12 04:47:54 +0000

    Add format_bilan_table() and tests

--- tests/testthat/test-read_zip_table_file.R
+++ tests/testthat/test-read_zip_table_file.R
@@ -4,7 +4,7 @@ test_that("unzip and save had dgf ", {
    %>% read_zip_table_file(champ = "had", statut = "dgf")
  )
 
-  withr::defer(fs::dir_delete("data"))
+  #withr::defer(fs::dir_delete("data"))
 
   rds_result_path <- "data/had_dgf/ovalide.rds"
   expected_file_size <- 2468142L
@@ -19,7 +19,7 @@ test_that("unzip and save psy oqn ", {
    %>% read_zip_table_file(champ = "psy", statut = "oqn")
  )
 
-  withr::defer(fs::dir_delete("data"))
+  #withr::defer(fs::dir_delete("data"))
 
   rds_result_path <- "data/psy_oqn/ovalide.rds"
   expected_file_size <- 3150629L
